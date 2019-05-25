1e03e0982323337c75623a4fd988c46e98b434df
Yip Cubed <yipcubed@gmail.com>
2019-05-25 21:22:44 +0000

    starting

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+/**
+ * Git instructions for copy-paste
+ * F1 to Open Shell
+
+git init; git add main.cpp; git config user.name "Yip Cubed"; git config user.email "yipcubed@gmail.com"; git config credential.helper 'cache --timeout 7200'; git commit -m "starting"
+
+git remote add origin https://yipcubed@github.com/yipcubed/hello-cpp.git
+git push -u origin master
+
+git add main.cpp; git commit -m "Made changes"
+git push -u origin master
+
+ *
+**/
+
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  
+  return 0;
+}
