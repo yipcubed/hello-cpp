942da756048c0ce003868223e9d3aefd5e2ed5cc
Yip <yipcubed@gmail.com>
2019-06-02 16:02:34 -0700

    added README.md to git list

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
  * Git instructions for copy-paste
  * F1 to Open Shell
 
-git init; git add main.cpp; git config user.name "Yip Cubed"; git config user.email "yipcubed@gmail.com"; git config credential.helper 'cache --timeout 7200'; git commit -m "starting"
+git init; git add main.cpp; git add README.md; git config user.name "Yip Cubed"; git config user.email "yipcubed@gmail.com"; git config credential.helper 'cache --timeout 7200'; git commit -m "starting"
 
 git remote add origin https://yipcubed@github.com/yipcubed/hello-cpp.git
 git push -u origin master
